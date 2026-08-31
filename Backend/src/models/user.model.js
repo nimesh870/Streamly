05ab130c05e7fb5decab7ece6399eb1973f1ae30
Nimesh Dulal <188695939+nimesh870@users.noreply.github.com>
2026-08-31 12:49:04 +0545

    Fixing typo in user models

--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -61,7 +61,7 @@ userSchema.pre("save" , async function (next) {
     this.password = await bcrypt.hash(this.password , 10);
 })
 
-userSchema.methods.ComparePassword = async function(password) {
+userSchema.methods.comparePassword = async function(password) {
     return await bcrypt.compare(password , this.password)
 }
 

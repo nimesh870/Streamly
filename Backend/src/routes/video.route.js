a4d53b3c586b9b4a8c2745a4993e935ab3f1a3bb
Nimesh Dulal <188695939+nimesh870@users.noreply.github.com>
2026-09-09 08:01:28 +0545

    writing routes for video controller

--- Backend/src/routes/video.route.js
+++ Backend/src/routes/video.route.js
@@ -1,11 +1,18 @@
 import { Router } from "express";
 import { upload } from "../middlewares/multer.middleware";
 import { verifyJWT } from "../middlewares/auth.middleware";
-import { publishVideo } from "../controllers/video.controller";
+import {
+     publishVideo,
+     deleteVideo,
+     updateVideo,
+     getVideoById,
+     getAllVideos,
+     togglePublished 
+    } from "../controllers/video.controller";
 
 const videoRouter = Router();
 
-router.route("/upload-videos").post(
+videoRouter.route("/upload-video").post(
     upload.fields([
         {
             name : "video",
@@ -18,5 +25,15 @@ router.route("/upload-videos").post(
     ]),
     verifyJWT , publishVideo
 )
+
+videoRouter.route("/:videoId").delete(verifyJWT , deleteVideo)
+
+videoRouter.route("/:videoId").patch(verifyJWT , upload.single("thumbnail") , updateVideo)
+
+videoRouter.route("/:videoId").get(getVideoById)
+
+videoRouter.route("/").get(getAllVideos)
+
+videoRouter.route("/:videoId/toggle-publish").patch(verifyJWT , togglePublished)
 
 export default videoRouter;

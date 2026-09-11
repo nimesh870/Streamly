0e8e582b673ddbf992cd368b7df7bd15491b3a2e
Nimesh Dulal <188695939+nimesh870@users.noreply.github.com>
2026-09-11 18:01:48 +0545

    writing create playlist controller.

---
+++ Backend/src/controllers/playlist.controller.js
@@ -0,0 +1,51 @@
+import { Playlist } from "../models/playlist.models.js"
+import { Video } from "../models/video.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { AsyncHandler } from "../utils/AsyncHandler.js";
+
+const createPlaylist = AsyncHandler(async (req, res) => {
+    const {name , description} = req.body;
+
+    if ( typeof name !== "string" || typeof description !== "string" || name?.trim() === "" || description?.trim() === "") {
+        throw new ApiError(400 , "Name and description are required.")
+    }
+
+    const playlist = await Playlist.create({
+        name : name.trim(),
+        description : description.trim(),
+        owner : req.user._id
+    })
+
+    return res.status(201).json(
+        new ApiResponse(201 , playlist , "Play successfully created.")
+    )
+})
+
+const getUserPlaylists = AsyncHandler(async (req, res) => {
+})
+
+const getPlaylistById = AsyncHandler(async (req, res) => {
+})
+
+const addVideoToPlaylist = AsyncHandler(async (req, res) => {
+})
+
+const removeVideoFromPlaylist = AsyncHandler(async (req, res) => {
+})
+
+const deletePlaylist = AsyncHandler(async (req, res) => {
+})
+
+const updatePlaylist = AsyncHandler(async (req, res) => {
+})
+
+export {
+    createPlaylist,
+    getUserPlaylists,
+    getPlaylistById,
+    addVideoToPlaylist,
+    removeVideoFromPlaylist,
+    deletePlaylist,
+    updatePlaylist
+}

72215ce9ae428efab600df4e247cda02d5f699ed
Nimesh Dulal <188695939+nimesh870@users.noreply.github.com>
2026-09-15 19:44:54 +0545

    controller to like video

--- Backend/src/controllers/like.controller.js
+++ Backend/src/controllers/like.controller.js
@@ -1,9 +1,10 @@
-import mongoose from "mongoose";
+import mongoose, { mongo } from "mongoose";
 import { Like } from "../models/likes.models.js";
 import { Comment } from "../models/comment.models.js"
 import { AsyncHandler } from "../utils/AsyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import { Video } from "../models/video.model.js";
 
 const toggleCommentLike = AsyncHandler( async (req , res) => {
     const { commentId } = req.params;
@@ -43,6 +44,43 @@ const toggleCommentLike = AsyncHandler( async (req , res) => {
     )
 })
 
+const toggleVideoId = AsyncHandler( async (req , res) => {
+    const { videoId } = req.params;
+
+    if (!videoId || !mongoose.Types.ObjectId.isValid(videoId)) {
+        throw new ApiError(400 , "Invalid video id.")
+    }
+
+    const video = await Video.findById(videoId);
+
+    const existingLike = await Like.findOne({
+        video : videoId,
+        likedBy : req.user._id
+    })
+
+    if (existingLike) {
+        await Like.deleteOne({
+            _id : existingLike?._id
+        })
+
+        return res.status(200).json(
+            new ApiResponse(200 , "Video unliked successfully.")
+        )
+    }
+
+    await Like.create({
+        video : videoId,
+        likedBy : req.user._id
+    })
+
+    return res.status(201).json(
+        new ApiResponse(201 , "Video liked successfully." , null)
+    )
+
+
+})
+
 export {
     toggleCommentLike,
+    toggleVideoId
 }

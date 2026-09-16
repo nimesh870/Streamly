6ba25a2013da0ffd2a1faa984f0e8e6095433df6
Nimesh Dulal <188695939+nimesh870@users.noreply.github.com>
2026-09-16 19:50:55 +0545

    writing controller for subscribing a channel

---
+++ Backend/src/controllers/subscription.controller.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { AsyncHandler } from "../utils/AsyncHandler.js";
+import { Subscription } from "../models/subscription.models.js";
+import { User } from "../models/user.model.js"
+
+const toggleSubscribe = AsyncHandler( async (req , res) => {
+    const { channelId } = req.params;
+
+    if (!channelId || !mongoose.Types.ObjectId.isValid(channelId)) {
+        throw new ApiError(400 , "Invalid channel id.")
+    
+    }
+
+    const existingSubscription = await Subscription.findOne({
+        subscriber : req.user._id,
+        channel : channelId
+    })
+
+    if (existingSubscription) {
+        await Subscription.deleteOne({
+            _id : existingSubscription._id
+        })
+
+        return res.status(200).json(
+            new ApiResponse(200 , "Channel unsubscribed successfully.")
+        )
+    }
+
+    await Subscription.create({
+        subscriber : req.user._id,
+        channel : channelId
+    })
+
+    return res.status(201).json(
+        new ApiResponse(201 , "Channel subscribed successfully.")
+    )
+
+})
+
+export {
+    toggleSubscribe,
+}

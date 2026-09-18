6635ccc47a7ad2ac3db2d6cb536f05a848eaf27e
Nimesh Dulal <188695939+nimesh870@users.noreply.github.com>
2026-09-18 19:39:28 +0545

    Adding tweet schema in comment model

--- Backend/src/models/comment.models.js
+++ Backend/src/models/comment.models.js
@@ -12,6 +12,11 @@ const commentSchema = new Schema({
         ref : "Video"
     },
 
+    tweet : {
+        type : Schema.Types.ObjectId,
+        ref : "Tweet"
+    },
+
     owner : {
         type : Schema.Types.ObjectId,
         ref : "User"

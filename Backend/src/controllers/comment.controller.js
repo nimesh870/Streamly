39587771602902ba1a66aeb40fcb64039789cdc3
Nimesh Dulal <188695939+nimesh870@users.noreply.github.com>
2026-09-18 21:40:24 +0545

    controller to fetch video comments

--- Backend/src/controllers/comment.controller.js
+++ Backend/src/controllers/comment.controller.js
@@ -137,9 +137,58 @@ const deleteComment = AsyncHandler( async (req , res) => {
     
 })
 
+const getVideoComments = AsyncHandler( async (req , res) => {
+    const { videoId } = req.params;
+
+    if (!videoId || !mongoose.Types.ObjectId.isValid(videoId)) {
+        throw new ApiError(400 , "Invalid video id.")
+    }
+
+    const video = await Video.findById(videoId);
+
+    if (!video) {
+        throw new ApiError(404 , "Video not found.")
+    }
+
+    const videoComments = await Comment.aggregate(
+        [
+            {
+                $match : {
+                    video : videoId,
+                }
+            },
+
+            {
+                $lookup : {
+                    from : "users",
+                    localField : "owner",
+                    foreignField : "_id",
+                    as : "ownerDetails"
+                }
+            },
+
+            {
+                $unwind : "$ownerDetails"
+            },
+        ]
+    )
+
+    if (videoComments.length === 0) {
+        return res.status(200).json(
+            new ApiResponse(200 , "No comments yet." , [])
+        )
+    }
+
+    return res.status(200).json(
+        new ApiResponse(200 , "Comments fetched successfully." , videoComments)
+    )
+    
+})
+
 export {
     addCommentToVideo,
     addCommentToTweet,
     updateComment,
-    deleteComment
+    deleteComment,
+    getVideoComments
 }

403e33fa58e7dc653abe47bf24a223804654a02e
Nimesh Dulal <188695939+nimesh870@users.noreply.github.com>
2026-09-23 21:34:30 +0545

    updating like route

--- Backend/src/routes/like.route.js
+++ Backend/src/routes/like.route.js
@@ -9,11 +9,11 @@ import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const likeRouter = Router();
 
-likeRouter.route("/:commentId").post(verifyJWT , toggleCommentLike);
+likeRouter.route("like-comment/:commentId").post(verifyJWT , toggleCommentLike);
 
-likeRouter.route("/:videoId").post(verifyJWT , toggleVideoLike);
+likeRouter.route("like-video/:videoId").post(verifyJWT , toggleVideoLike);
 
-likeRouter.route("/:tweetId").post(verifyJWT , toggleTweetLike);
+likeRouter.route("like-tweet/:tweetId").post(verifyJWT , toggleTweetLike);
 
 likeRouter.route("/liked-videos").get(verifyJWT , getLikedVideos);
 

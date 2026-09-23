23f1e403e38318425fea840ef2b4ac6c813de93b
Nimesh Dulal <188695939+nimesh870@users.noreply.github.com>
2026-09-23 21:36:12 +0545

    Fixing route errors

--- Backend/src/routes/like.route.js
+++ Backend/src/routes/like.route.js
@@ -9,11 +9,11 @@ import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const likeRouter = Router();
 
-likeRouter.route("like-comment/:commentId").post(verifyJWT , toggleCommentLike);
+likeRouter.route("/like-comment/:commentId").post(verifyJWT , toggleCommentLike);
 
-likeRouter.route("like-video/:videoId").post(verifyJWT , toggleVideoLike);
+likeRouter.route("/like-video/:videoId").post(verifyJWT , toggleVideoLike);
 
-likeRouter.route("like-tweet/:tweetId").post(verifyJWT , toggleTweetLike);
+likeRouter.route("/like-tweet/:tweetId").post(verifyJWT , toggleTweetLike);
 
 likeRouter.route("/liked-videos").get(verifyJWT , getLikedVideos);
 
